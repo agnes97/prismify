481d2ad922fa713358c8e5453b560d6654da18c0
agnes97 <jana@macovi.space>
2024-02-04 20:06:30 +0100

    🎨 Install and apply Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import * as path from 'path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
+import * as path from 'path'
+import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload'
+import { FastifyPluginAsync } from 'fastify'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -8,16 +8,14 @@ const __dirname = path.dirname(__filename)
 
 export type AppOptions = {
   // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
-
+} & Partial<AutoloadPluginOptions>
 
 // Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {
-}
+const options: AppOptions = {}
 
 const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
+  fastify,
+  opts,
 ): Promise<void> => {
   // Place here your custom code!
 
@@ -29,7 +27,7 @@ const app: FastifyPluginAsync<AppOptions> = async (
   void fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: opts,
-    forceESM: true
+    forceESM: true,
   })
 
   // This loads all plugins defined in routes
@@ -37,10 +35,9 @@ const app: FastifyPluginAsync<AppOptions> = async (
   void fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: opts,
-    forceESM: true
+    forceESM: true,
   })
+}
 
-};
-
-export default app;
+export default app
 export { app, options }
--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -1,4 +1,4 @@
-import { FastifyPluginAsync } from "fastify"
+import { FastifyPluginAsync } from 'fastify'
 
 const example: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
   fastify.get('/', async function (_request, _reply) {
@@ -6,4 +6,4 @@ const example: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
   })
 }
 
-export default example;
+export default example

7419e4529110285fb4110277274d7512cdf1502f
agnes97 <jana@macovi.space>
2024-10-19 17:32:31 +0200

    ✨ Doggo time gets name from querystring

--- src/routes/make/index.ts
+++ src/routes/make/index.ts
@@ -2,15 +2,17 @@ import { FastifyPluginAsync } from 'fastify'
 import axios from 'axios'
 
 const doggoTime: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
-  fastify.get('/doggo-time', async function (request, _reply) {
-    await axios.post(
-      'https://hook.eu2.make.com/jg3b9xpstdq8r305qoi737ba1h9c9hy6',
-      {
-        name: 'Henry',
-        request: request.body,
-      },
-    )
-  })
+  fastify.get<{ Querystring: { name: string } }>(
+    '/doggo-time',
+    async function (request, _reply) {
+      await axios.post(
+        'https://hook.eu2.make.com/jg3b9xpstdq8r305qoi737ba1h9c9hy6',
+        {
+          name: request.query.name,
+        },
+      )
+    },
+  )
 }
 
 export default doggoTime
